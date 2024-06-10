a9daa692f1c3341b8183166e2a986d7cb32c0ea6
astonuser <sabibabiba7@gmail.com>
2024-06-10 17:35:08 +0300

    12

--- src/test/java/org/example/TestFactorial.java
+++ src/test/java/org/example/TestFactorial.java
@@ -2,13 +2,12 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class TestFactorial {
-        @Test
-        public void testFactorial() {
-            assertEquals("Факториал числа 5 должен быть равен 120", 120, Factorial.factorial(5));
-            assertEquals("Факториал числа 0 должен быть равен 1", 1, Factorial.factorial(0));
-            assertEquals("Факториал числа 1 должен быть равен 1", 1, Factorial.factorial(1));
-            assertEquals("Факториал числа 3 должен быть равен 6", 6, Factorial.factorial(3));
-        }
+    @Test
+    public void testFactorial() {
+        assertEquals("Факториал числа 5 должен быть равен 120", 120, Factorial.factorial(5));
+        assertEquals("Факториал числа 0 должен быть равен 1", 1, Factorial.factorial(0));
+        assertEquals("Факториал числа 1 должен быть равен 1", 1, Factorial.factorial(1));
+        assertEquals("Факториал числа 3 должен быть равен 6", 6, Factorial.factorial(3));
     }
-
 }
+
