906f5eded8d2f605e135fa7b6896517fc21e9dec
astonuser <sabibabiba7@gmail.com>
2024-06-10 17:31:42 +0300

    12

---
+++ src/main/java/org/example/Factorial.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Factorial {
+    static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                + factorial(5));
+    }
+}
